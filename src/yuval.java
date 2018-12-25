f271e0d9232867612d15a0ed1a357598e1d078f9
YuvalAmar <yuvalamar75@gmail.com>
2018-12-25 21:01:23 +0200

    gg

---
+++ src/yuval.java
@@ -0,0 +1,9 @@
+public class yuval {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+    public void static yuvalf(){
+        System.out.println("yuvall");
+    }
+}
