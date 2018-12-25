beec0fe72649fb6505f0b0e6e0fff8bf14518251
1234 <dvirhacohen@gmail.com>
2018-12-25 21:21:25 +0200

    Reset

--- src/yuval.java
+++
@@ -1,9 +0,0 @@
-public class yuval {
-    public static void main(String[] args) {
-        System.out.println("test");
-    }
-
-    public static void  yuvalf(){
-        System.out.println("yuvall");
-    }
-}
