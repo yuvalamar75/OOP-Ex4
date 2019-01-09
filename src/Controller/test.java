fc7898d1aa88290ba1da0b7a252c6f74e1918e8a
YuvalAmar <yuvalamar75@gmail.com>
2019-01-09 12:12:56 +0200

    SQL stats

--- src/Controller/test.java
+++ src/Controller/test.java
@@ -8,7 +8,7 @@ import GUI.SQLDisplay;
 public class test {
     public static void main(String[] args) {
        Controller control =  new Controller();
-        SQL sql = new SQL();
+        //SQL sql = new SQL();
 
     }
 }
