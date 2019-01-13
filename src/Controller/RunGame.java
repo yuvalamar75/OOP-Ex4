ab9d97c61222f7cd165c607763c6af62419c114a
1234 <dvirhacohen@gmail.com>
2019-01-13 13:42:55 +0200

    final commit - game is ready

---
+++ src/Controller/RunGame.java
@@ -0,0 +1,12 @@
+package Controller;
+
+public class RunGame {
+    /**
+     *
+     * @param args start the game
+     */
+    public static void main(String[] args) {
+        Controller control = new Controller();
+        control.startGame();
+    }
+}
